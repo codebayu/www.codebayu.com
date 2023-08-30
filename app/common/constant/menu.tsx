05c6654563a99f962bbd12a67d56e395f3aa1623
Bayu Setiawan <bayusetiawanipb@gmail.com>
2023-08-30 11:55:50 +0700

    remove unused code

--- app/common/constant/menu.tsx
+++ app/common/constant/menu.tsx
@@ -157,14 +157,3 @@ export const SOCIAL_MEDIA: MenuItemProps[] = [
     eventName: 'Social: Tiktok',
   },
 ];
-
-export const EXTERNAL_LINKS: MenuItemProps[] = [
-  {
-    title: 'Analytics',
-    href: 'https://umami.aulianza.id/share/LK5kiRuosw9pORLa/aulianza.id',
-    icon: <AnalyticsIcon size={iconSize} />,
-    isShow: true,
-    isExternal: true,
-    eventName: 'External Link: Analytics',
-  },
-];
