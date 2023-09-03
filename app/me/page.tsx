309371a8fd9f0ed8e0e040d9ffa725e16eee7c21
Bayu Setiawan <bayusetiawanipb@gmail.com>
2023-09-03 10:26:44 +0700

    add metadata to me page

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,6 +1,19 @@
+import { Metadata } from 'next';
+
 import React from 'react';
 
+import { METADATA } from '@/common/constant/metadata';
+
 import MeSection from '@/modules/me';
+
+export const metadata: Metadata = {
+  title: `Hi! ${METADATA.exTitle}`,
+  description: 'Programming tips for software developer',
+  keywords: 'frontend developer, software engineer, react js, javascript, typescript',
+  alternates: {
+    canonical: `${process.env.DOMAIN}/me`
+  }
+};
 
 export default function MePage() {
   return <MeSection />;
