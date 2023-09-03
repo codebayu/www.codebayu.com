1125634b42f7b8f678cbf675787c0538005d1705
Bayu Setiawan <bayusetiawanipb@gmail.com>
2023-09-03 12:17:22 +0700

    fix height skeleton pagespeed

--- common/components/elements/LoadingSpeedInsight.tsx
+++ common/components/elements/LoadingSpeedInsight.tsx
@@ -6,7 +6,7 @@ export default function LoadingSpeedInsight() {
     <div className="my-6 flex text-neutral-600 md:text-sm justify-center items-end space-x-6 md:space-x-10">
       {[1, 2, 3, 4].map(item => (
         <div key={item} className="flex flex-col w-max text-center items-center justify-center space-y-2">
-          <div className="h-2 w-16 md:w-[82px] bg-neutral-300 dark:bg-neutral-700 animate-pulse rounded-full"></div>
+          <div className="h-3 w-16 md:w-[82px] bg-neutral-300 dark:bg-neutral-700 animate-pulse rounded-full"></div>
           <div className="w-12 md:w-16 h-12 md:h-16 p-[6px] bg-neutral-300 dark:bg-neutral-700 animate-pulse rounded-full">
             <div className="w-full h-full rounded-full bg-white dark:bg-neutral-900 flex items-center justify-center">
               <BsThreeDots />
