ff1906831ffbc75f36b22d5cffb69ad52f52a970
Bayu Setiawan <bayusetiawanipb@gmail.com>
2023-09-07 11:17:44 +0700

    add nextjs props and state

--- common/components/layouts/index.tsx
+++ common/components/layouts/index.tsx
@@ -29,7 +29,7 @@ export default function Layouts({ children }: LayoutsProps) {
             <Sidebar />
           </header>
         )}
-        <main className="lg:max-w-[854px] transition-all duration-300 w-full lg:h-screen overflow-y-auto no-scrollbar">
+        <main className="lg:max-w-[854px] transition-all duration-300 w-full lg:min-h-screen overflow-y-auto no-scrollbar">
           {children}
         </main>
       </div>
