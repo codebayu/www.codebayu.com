f0965152000009bdb66031dda46354b893fd3c6b
Bayu Setiawan <bayusetiawanipb@gmail.com>
2023-09-16 11:12:51 +0700

    installation, configuration, and sample implementation

---
+++ common/helpers/__test__/index.spec.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from 'vitest';
+
+import { formatBlogSlug, formatDate } from '..';
+
+describe('Helper Function', () => {
+  it('Should running formatDate function successfully', () => {
+    const formatDateFunction = formatDate('2023-09-12T02:59:33Z');
+    expect(formatDateFunction).toBe('September 12, 2023');
+  });
+
+  it('Should running formatBlogSlug function successfully', () => {
+    const formatBlogSlugFunction = formatBlogSlug('spas-vs-mpas-in-web-development-4cd8');
+    expect(formatBlogSlugFunction).toBe('spas-vs-mpas-in-web-development');
+  });
+});
