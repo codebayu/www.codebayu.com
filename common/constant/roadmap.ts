94cf54e1bea2320edf1e8a49cf3b9a56560f8f34
Bayu Setiawan <bayusetiawanipb@gmail.com>
2023-09-20 23:17:55 +0700

    create mastering react roadmap

--- common/constant/roadmap.ts
+++ common/constant/roadmap.ts
@@ -100,9 +100,115 @@ const BACKEND = [
   }
 ];
 
+const MASTERING_REACT = [
+  {
+    title: 'Fundamental of React',
+    icon: 'React.js',
+    linkIndonesia: 'https://www.youtube.com/watch?v=5kHyviqjhCk&list=PLU4DS8KR-LJ03qEsHn9zV4qdhcWtusBqb',
+    linkEnglish: 'https://www.youtube.com/watch?v=QFaFIcGhPoM&list=PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3'
+  },
+  {
+    title: 'React Hooks',
+    icon: 'React.js',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=cF2lQ_gZeA8&list=PLC3y8-rFHvwisvxhZ135pogtX7_Oe3Q3A'
+  },
+  {
+    title: 'React Router',
+    icon: 'React Router',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=UWYOC8g5N_0&list=PLC3y8-rFHvwjkxt8TOteFdT_YmzwpBlrG'
+  },
+  {
+    title: 'React Redux',
+    icon: 'Redux',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=9boMnm5X9ak&list=PLC3y8-rFHvwheJHvseC3I0HuYI2f46oAK'
+  },
+  {
+    title: 'Practical React',
+    icon: 'React.js',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=LZhwNGpiTEI&list=PLC3y8-rFHvwhAh1ypBvcZLDO6I7QTY5CM'
+  },
+  {
+    title: 'React Formik',
+    icon: 'React.js',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=a94FOvaBomQ&list=PLC3y8-rFHvwiPmFbtzEWjESkqBVDbdgGu'
+  },
+  {
+    title: 'React Storybook',
+    icon: 'Storybook',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=BySFuXgG-ow&list=PLC3y8-rFHvwhC-j3x3t9la8-GQJGViDQk'
+  },
+  {
+    title: 'React Render',
+    icon: 'React.js',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=YP2f-ErXG_M&list=PLC3y8-rFHvwg7czgqpQIBEAHn8D6l530t'
+  },
+  {
+    title: 'React Table',
+    icon: 'React Table',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=YwP4NAZGskg&list=PLC3y8-rFHvwgWTSrDiwmUsl4ZvipOw9Cz'
+  },
+  {
+    title: 'React Typescript',
+    icon: 'React.js',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=TiSGujM22OI&list=PLC3y8-rFHvwi1AXijGTKM0BKtHzVC-LSK'
+  },
+  {
+    title: 'React Query',
+    icon: 'React Query',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=VtWkSCZX0Ec&list=PLC3y8-rFHvwjTELCrPrcZlo6blLBUspd2'
+  },
+  {
+    title: 'React Material UI',
+    icon: 'React.js',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=BHEPVdfBAqE&list=PLC3y8-rFHvwh-K9mDlrrcDywl7CeVL2rO'
+  },
+  {
+    title: 'React Redux Toolkit',
+    icon: 'React.js',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=0awA5Uw6SJE&list=PLC3y8-rFHvwiaOAuTtVXittwybYIorRB3'
+  },
+  {
+    title: 'React Hook Form',
+    icon: 'React Hook Form',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=KejZXxFCe2k&list=PLC3y8-rFHvwjmgBr1327BA5bVXoQH-w5s'
+  },
+  {
+    title: 'React Testing',
+    icon: 'React.js',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=T2sv8jXoP4s&list=PLC3y8-rFHvwirqe1KHFCHJ0RqNuN61SJd'
+  },
+  {
+    title: 'React Native',
+    icon: 'React Native',
+    linkIndonesia: '',
+    linkEnglish: 'https://www.youtube.com/watch?v=hzzCveeczSQ&list=PLC3y8-rFHvwhiQJD1di4eRVN30WWCXkg1'
+  },
+  {
+    title: 'Next Js',
+    icon: 'Next.js',
+    linkIndonesia: 'https://www.youtube.com/watch?v=HNXYveOJPio&list=PLU4DS8KR-LJ3-zouYHHknPq1G5VTB8PRf',
+    linkEnglish: 'https://www.youtube.com/watch?v=RY6B7JSBRRg&list=PLC3y8-rFHvwgC9mj0qv972IO5DmD-H0ZH&index=2'
+  }
+];
+
 export const ROADMAP = {
   frontend: FRONTEND,
-  backend: BACKEND
+  backend: BACKEND,
+  masteringReact: MASTERING_REACT
 };
 
 export const TRIBES = [
@@ -115,5 +221,10 @@ export const TRIBES = [
     label: 'Backend Developer',
     path: '/roadmap?tribe=backend-developer',
     isShow: false
+  },
+  {
+    label: 'Mastering React Js',
+    path: '/roadmap?tribe=mastering-react-js',
+    isShow: true
   }
 ];
