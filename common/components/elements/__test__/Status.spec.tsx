db680b22bfc205e31684fd1e1e68dda1dea52a56
Bayu Setiawan <bayusetiawanipb@gmail.com>
2023-10-01 14:52:51 +0700

    update unit test

--- common/components/elements/__test__/Status.spec.tsx
+++ common/components/elements/__test__/Status.spec.tsx
@@ -11,6 +11,8 @@ describe('Status Component', () => {
   it('Should render available element', () => {
     const dots = screen.getByTestId('available-hire');
     expect(dots).toBeTruthy();
-    expect(dots.className).toBe('flex items-center gap-2');
+    expect(dots.className).toBe(
+      'flex relative items-center gap-2 bg-white dark:bg-dark rounded-xl py-1 px-2 border border-neutral-300 dark:border-neutral-700'
+    );
   });
 });
