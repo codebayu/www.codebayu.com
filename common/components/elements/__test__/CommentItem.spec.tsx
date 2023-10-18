dfd4c46c503ad91918b172fc5ef65eea403bc21b
Bayu Setiawan <bayusetiawanipb@gmail.com>
2023-10-18 19:24:52 +0700

    update unit test for some components

---
+++ common/components/elements/__test__/CommentItem.spec.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable @next/next/no-img-element */
+import { render, screen } from '@testing-library/react'
+import { ClassAttributes, ImgHTMLAttributes } from 'react'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+
+import { formatDate } from '@/common/helpers'
+import { commentsMock } from '@/common/mocks/blog'
+
+import CommentItem from '../CommentItem'
+
+vi.mock('next/image', () => ({
+  __esModule: true,
+  default: (
+    props: JSX.IntrinsicAttributes & ClassAttributes<HTMLImageElement> & ImgHTMLAttributes<HTMLImageElement>
+  ) => <img {...props} />
+}))
+
+describe('CommentItem Component', () => {
+  beforeEach(() => {
+    render(<CommentItem {...commentsMock[0]} />)
+  })
+
+  it('Should render coment item', () => {
+    const container = screen.getByTestId('comment-item')
+    expect(container).toBeTruthy()
+    expect(container.className).toBe('flex gap-5 dark:text-neutral-400 break-all')
+  })
+
+  it('Should render user image', () => {
+    const image = screen.getAllByTestId('user-comment-image')[0]
+    expect(image).toBeTruthy()
+    expect(image.className).toContain('border border-neutral-200 dark:border-neutral-800')
+    expect(image).toHaveProperty('src', commentsMock[0].user?.profile_image_90)
+    expect(image).toHaveProperty('alt', commentsMock[0].user?.name)
+  })
+
+  it('Should render username', () => {
+    const username = screen.getAllByText(commentsMock[0].user?.name)[0]
+    expect(username).toBeTruthy()
+    expect(username.className).toBe('font-medium dark:text-neutral-300')
+  })
+
+  it('Should render dot', () => {
+    const dot = screen.getAllByText('•')[0]
+    expect(dot).toBeTruthy()
+    expect(dot.className).toBe('hidden sm:block dark:text-neutral-700')
+  })
+
+  it('Should render comment date', () => {
+    const commentDate = screen.getAllByText(formatDate(commentsMock[0].created_at, 'MMM dd, yyyy, HH:mm'))[0]
+    expect(commentDate).toBeTruthy()
+    expect(commentDate.className).toBe('text-xs dark:text-neutral-500')
+  })
+
+  it('Should render comment body', () => {
+    const commentBody = screen.getAllByTestId('comment-body')[0]
+    expect(commentBody).toBeTruthy()
+    expect(commentBody.className).toBe('leading-[1.8] max-w-[600px]')
+  })
+})
--- common/components/elements/__test__/Status.spec.tsx
+++ common/components/elements/__test__/Status.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { beforeEach, describe, expect, it } from 'vitest'
 
 import Status from '../Status'
@@ -8,11 +8,62 @@ describe('Status Component', () => {
     render(<Status />)
   })
 
+  it('Should render status container element', () => {
+    const element = screen.getByTestId('available-hire-container')
+    expect(element).toBeTruthy()
+    expect(element.className).toBe(
+      'absolute inverted-border-radius-dark z-10 left-0 py-2 pr-2 rounded-br-xl bg-white dark:bg-dark'
+    )
+  })
+
+  it('Should render status container element light', async () => {
+    const element = screen.getAllByTestId('available-hire-container')[0]
+    waitFor(() => {
+      expect(element).toBeTruthy()
+      expect(element.className).toBe(
+        'absolute inverted-border-radius z-10 left-0 py-2 pr-2 rounded-br-xl bg-white dark:bg-dark'
+      )
+    })
+  })
+
   it('Should render available element', () => {
-    const dots = screen.getByTestId('available-hire')
-    expect(dots).toBeTruthy()
-    expect(dots.className).toBe(
+    const element = screen.getAllByTestId('available-hire')[0]
+    expect(element).toBeTruthy()
+    expect(element.className).toBe(
       'flex relative items-center gap-2 bg-white dark:bg-dark rounded-xl py-1 px-2 border border-neutral-300 dark:border-neutral-700'
     )
+  })
+
+  it('Should render available element', () => {
+    const dots = screen.getAllByTestId('dots')[0]
+    expect(dots).toBeTruthy()
+    expect(dots.className).toBe('h-2 w-2 rounded-full bg-green-400')
+  })
+
+  it('Should render Hire Me text', () => {
+    const hire = screen.getAllByText('Hire me.')[0]
+    expect(hire).toBeTruthy()
+    expect(hire.className).toBe('text-xs text-neutral-600 dark:text-neutral-400')
+  })
+
+  it('Should not render available element when not mounted', async () => {
+    const element = screen.getAllByTestId('available-hire')
+    waitFor(() => {
+      expect(element).toBeFalsy()
+    })
+  })
+
+  it('Should not render dot element when not mounted', async () => {
+    const dots = screen.getAllByTestId('dots')[0]
+    waitFor(() => {
+      expect(dots).toBeFalsy()
+    })
+  })
+
+  it('Should not render Hire Me text when not mounted', async () => {
+    const hire = screen.getAllByText('Hire me.')[0]
+    waitFor(() => {
+      expect(hire).toBeFalsy()
+    })
   })
 })
