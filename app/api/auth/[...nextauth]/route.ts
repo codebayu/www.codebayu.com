4d3ff1764ef6971a5745679e51aac29622c35316
Bayu Setiawan <bayusetiawanipb@gmail.com>
2024-01-03 02:46:44 +0700

    style(prettier): fix prettier error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
-import NextAuth from 'next-auth';
-import { options } from './options';
+import NextAuth from 'next-auth'
 
-const handler = NextAuth(options);
+import { options } from './options'
 
-export { handler as GET, handler as POST };
+const handler = NextAuth(options)
+
+export { handler as GET, handler as POST }
