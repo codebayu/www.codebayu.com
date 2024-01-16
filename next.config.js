b451dcf8485f6714669e8fc0427745e7d08ad8cb
Bayu Setiawan <bayusetiawanipb@gmail.com>
2024-01-16 16:25:27 +0700

    chore(version): update outdated dependencies version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['picsum.photos', 'res.cloudinary.com', 'lh3.googleusercontent.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos'
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
+      }
+    ]
   },
   env: {
     GTM_ID: process.env.GTM_ID
