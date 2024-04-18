0266e7c3b4158162248773fbdfc8c8aad34e557e
Bayu Setiawan <bayusetiawanipb@gmail.com>
2024-04-18 19:08:34 +0700

    fix(ads): add ads.txt adn meta tag

--- components/elements/GoogleAdsense.tsx
+++ components/elements/GoogleAdsense.tsx
@@ -3,10 +3,13 @@
 export default function GoogleAdsense() {
   const googleAdsUnitBlogClient = process.env.GOOGLE_ADSENSE_UNIT_BLOG_CLIENT
   return (
-    <script
-      async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${googleAdsUnitBlogClient}`}
-      crossOrigin="anonymous"
-    ></script>
+    <>
+      <script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${googleAdsUnitBlogClient}`}
+        crossOrigin="anonymous"
+      ></script>
+      <meta name="google-adsense-account" content="ca-pub-3509410750346487"></meta>
+    </>
   )
 }
