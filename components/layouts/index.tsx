47ac1d2bb5c813244d7990ae924316211fc7f349
Bayu Setiawan <bayusetiawanipb@gmail.com>
2024-08-04 14:26:29 +0700

    refactor(layout): takedown star animation

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -10,7 +10,6 @@ import { ReactNode, useEffect } from 'react'
 import ChatOverlay from '../elements/ChatOverlay'
 import RunningText from '../elements/RunningText'
 import ShiningStar from '../elements/ShiningStar'
-import ShootingStar from '../elements/ShootingStar'
 import LeftCollapseNavigation from './LeftCollapseNavigation'
 import MobileHeader from './MobileHeader'
 
@@ -35,7 +34,6 @@ export default function Layouts({ children }: LayoutsProps) {
   }, [])
   return (
     <div className="flex flex-col justify-center overflow-x-hidden">
-      <ShootingStar />
       <ShiningStar />
       <div className="flex w-full flex-col justify-center lg:flex-row lg:gap-5">
         {!hideSidebar && (
