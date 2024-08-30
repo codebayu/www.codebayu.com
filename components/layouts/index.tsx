4f09b86022834ba4be65b205a40c1fd7042d6d55
Bayu Setiawan <bayusetiawanipb@gmail.com>
2024-08-30 20:55:21 +0700

    feat(layout): create box grid background

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -33,7 +33,7 @@ export default function Layouts({ children }: LayoutsProps) {
     })
   }, [])
   return (
-    <div className="flex flex-col justify-center overflow-x-hidden">
+    <div className="flex h-full w-full flex-col justify-center overflow-x-hidden">
       <ShiningStar />
       <div className="flex w-full flex-col justify-center lg:flex-row lg:gap-5">
         {!hideSidebar && (
@@ -41,10 +41,12 @@ export default function Layouts({ children }: LayoutsProps) {
             <MobileHeader /> <RunningText />
           </>
         )}
-        <main className="no-scrollbar w-full scroll-smooth transition-all duration-300 lg:ml-10 lg:min-h-screen lg:max-w-[854px]">
+        <main className="no-scrollbar h-full w-full scroll-smooth transition-all duration-300 lg:ml-10 lg:min-h-screen lg:max-w-[854px]">
           {children}
         </main>
+        <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:30px_30px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_0%,#000_80%,transparent_100%)]"></div>
       </div>
+
       {!hideSidebar && <LeftCollapseNavigation />}
       <Notif />
       <ChatOverlay />
