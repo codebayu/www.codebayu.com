8c5e550667b6225a8889f16fd076efcd71e527f5
Bayu Setiawan <bayusetiawanipb@gmail.com>
2024-08-30 20:22:02 +0700

    feat(ads): change ads banner styles

--- components/elements/AdsPlaceholder.tsx
+++ components/elements/AdsPlaceholder.tsx
@@ -11,20 +11,19 @@ interface AdsBannerProps {
   data: IAdsBanner
 }
 
-export default function AdsBanner({ className = 'w-72 h-full', data }: AdsBannerProps) {
-  const fontSize = data.text.length < 40 ? 'text-xs' : 'text-[10px]'
+export default function AdsBanner({ className = 'h-full', data }: AdsBannerProps) {
+  const fontSize = data.text.length < 40 ? 'text-xs md:text-sm' : 'text-[10px] md:text-xs'
   return data.isShow ? (
     <Link
       href={data.link}
       className={cn(
-        'rounded-smfont-sans relative flex justify-center overflow-hidden rounded-sm border border-sky-100 bg-sky-50 text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:text-neutral-300 md:justify-start',
+        'glow-button text-neutral-800 after:bg-white dark:text-white dark:after:bg-dark',
         className,
         fontSize
       )}
       target="_blank"
     >
-      <div className="absolute h-full w-full animate-pulse bg-gradient-to-br from-sky-100 to-sky-200 dark:from-neutral-800 dark:to-neutral-900"></div>
-      <div className="relative flex items-center space-x-2 px-2 py-1">
+      <div className="flex items-center space-x-2 px-3 py-1">
         <Image src={data.image} alt="promote icon" width={30} height={30} />
         <p>{data.text || 'Iklan'}</p>
       </div>
