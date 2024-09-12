f9c6f803b5cacbf6d3769c5703440c146b1b2a1f
Bayu Setiawan <bayusetiawanipb@gmail.com>
2024-09-12 18:36:15 +0700

    feat(home): decrease grid background saturation

--- modules/home/components/ServicesCard.tsx
+++ modules/home/components/ServicesCard.tsx
@@ -27,7 +27,7 @@ export default function ServicesCard({ tag, title, description, id }: ServicesCa
         <h3 className="text-xl font-medium">{title}</h3>
         <p className="text-sm text-neutral-600 dark:text-neutral-400">{description}</p>
       </div>
-      <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#80808019_1px,transparent_1px),linear-gradient(to_bottom,#80808019_1px,transparent_1px)] bg-[size:18px_18px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_20%,#000_80%,transparent_100%)]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#80808018_1px,transparent_1px),linear-gradient(to_bottom,#80808018_1px,transparent_1px)] bg-[size:18px_18px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_20%,#000_80%,transparent_100%)]"></div>
     </Card>
   )
 }
